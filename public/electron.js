907e8f3e7473f23dd47afb63e78c5b0cae8aa9b9
Roy Prigat <rp2719@columbia.edu>
2018-03-07 15:04:42 -0500

    moved electron.js to public

---
+++ public/electron.js
@@ -0,0 +1,42 @@
+// This file is in the “public” folder instead of “src” so
+// it can get copied to the “build” folder as it is
+
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 900, height: 600, titleBarStyle: 'hidden' });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  app.on('ready', createWindow);
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });
+}
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here
